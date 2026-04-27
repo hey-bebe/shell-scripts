8b417ff8836917c647e9d13199effdf13306e8ef
Cody Edwards <cody.edwards@mm.st>
2026-04-27 15:57:30 -0600

    Add fts.sh TODOs

--- fts.sh
+++ fts.sh
@@ -4,19 +4,20 @@
 # defined file extension and displays the total number of those files
 # and their collective size in MB.
 
+#TODO add ability to select specific directory
+#TODO add max-depth
+#TODO add ability to ignore certain directories
+#TODO use getopts
+
 extension=$1
 
 if [ -z "$extension" ]; then
     echo "You did not enter a file extension"
 else
     find ~ -type f -name "*.$extension" -print0 \
-	| xargs -0 -r du -k \	# the -r flag prevents xargs from running if find returns nothing
-	| awk '{
-	      total+=$1
-   	       }
-
+	| xargs -0 du -k \
+	| awk '{ total+=$1 }
 	           END {
 		         printf("\nTotal files: %d\nTotal Size: %.1f MB\n", NR, total/1024)
-			     }
-			       '
+			 }'
 fi 
