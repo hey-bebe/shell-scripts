cc6da1c038d487ac201ef35e2430357679376fa4
Cody Edwards <cody.edwards@mm.st>
2026-04-28 11:53:13 -0600

    changed shebang to /usr/bin/env bash; refactored awk statements

--- fts.sh
+++ fts.sh
@@ -1,27 +1,30 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# This script recursively searches the home directory for a user
-# defined file extension and displays the total number of those files
-# and their collective size in MB.
+# This script recursively searches the home directory by default for a
+# user defined file extension and displays the total number of that
+# file type and the total size of those files in MB. Use the -d flag
+# to specifiy a directory other than the home directory. Use the -f
+# flag to use fd, which is substantially faster than find.
 
 #TODO add max-depth
 #TODO add option to ignore certain directories
 #TODO add help
+#TODO add support for smaller file sizes
 
 directory="$HOME"
 extension=""
-utility=false
+fd=false
 
 while getopts "e:d:f" opt; do
     case "$opt" in
 	e)			# specify an extension
-	    extension=$OPTARG
+	    extension=${OPTARG#.}
 	    ;;
 	d)			# specifiy a directory
 	    directory=$OPTARG
 	    ;;
 	f)			# Use fd instead of find
-	    utility=true
+	    fd=true
 	    ;;
 	*)
 	    echo "Usage: $0 -e extension [-d directory] [-f]"
@@ -35,20 +38,34 @@ if [ -z "$extension" ]; then
     echo "Usage: ./fts.sh -e <extension> [-d <dirctory>] [-f]"
     exit 1
 
-elif [ "$utility" = true ]; then  
+elif [ "$fd" = true ]; then  
 
     fd -u -0 -t f -e "$extension" . "$directory" \
     | xargs -0 -r du -k \
-    | awk '{total+=$1}
-		END {
-		printf("\nTotal files: %d\nTotal Size: %.1f MB\n", NR, total/1024)
-		}'
+    | awk -v extension="$extension" '
+	{
+	    total+=$1
+	}
+	END {
+	    printf("Total files (%s): %d\n" \
+	    	   "Total Size: %.1f MB\n\n",
+		   extension,
+		   NR,
+		   total/1024)
+	}'
 
 else
     find "$directory" -type f -iname "*.$extension" -print0 \
     | xargs -0 -r du -k \
-    | awk '{total+=$1}
-        END {
-		printf("\nTotal files: %d\nTotal Size: %.1f MB\n", NR, total/1024)
-		}'
+    | awk -v extension="$extension" '
+        {
+	    total+=$1
+	}
+	END {
+	    printf("Total files (%s): %d\n" \
+	    	   "Total Size: %.1f MB\n\n",
+		   extension,
+		   NR,
+		   total/1024)
+	}'
 fi 
